357dbe9e7fc05a3f8d068db25bafa8ad3ef89a2d
Thomas So <thomas.m.so33@gmail.com>
2018-03-12 19:53:32 -0700

    Initial implementation

---
+++ Unity Plugin Demo/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿//
+//  MainMenu.cs
+//  Unity Plugin Demo
+//
+//  Created by Thomas So on 3/12/18.
+//  Copyright © 2018 AppLovin. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	void Start ()
+	{
+		Screen.orientation = ScreenOrientation.Portrait;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
