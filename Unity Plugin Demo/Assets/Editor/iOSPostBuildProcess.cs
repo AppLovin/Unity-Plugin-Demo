2ebe926a44b045acb37654a68326a38c2bede573
Chris Cong <christopher.cong@applovin.com>
2018-04-26 15:09:57 -0700

    Add -ObjC linker flag

---
+++ Unity Plugin Demo/Assets/Editor/iOSPostBuildProcess.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class BuildPostProcessor
+{
+	[PostProcessBuildAttribute(1)]
+	public static void OnPostProcessBuild(BuildTarget target, string path)
+	{
+		if (target == BuildTarget.iOS)
+		{
+			string projectPath = PBXProject.GetPBXProjectPath(path);
+			PBXProject project = new PBXProject();
+			project.ReadFromString(File.ReadAllText(projectPath));
+
+			string targetName = PBXProject.GetUnityTargetName();
+			string targetGUID = project.TargetGuidByName(targetName);
+
+			// Add `-ObjC` to "Other Linker Flags".
+			project.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-ObjC");
+
+			// Write.
+			File.WriteAllText(projectPath, project.WriteToString());
+		}
+	}
+}
